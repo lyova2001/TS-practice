37ea79ca28830fe0b04ef41974237d0536ddfe57
Lyova-FC <lyovaavetisyan31@gmail.com>
2022-02-21 15:14:56 +0300

    Validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,52 @@
-function autoBind(
-  target: any,
-  methodName: string,
-  descriptor: PropertyDescriptor
-) {
+// Validation
+interface Validatable {
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max
+  }
+  return isValid
+}
+
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
-
     get() {
       const boundFn = originalMethod.bind(this)
       return boundFn
@@ -15,6 +55,7 @@ function autoBind(
   return adjDescriptor
 }
 
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
@@ -26,8 +67,8 @@ class ProjectInput {
   constructor() {
     this.templateElement = document.getElementById(
       'project-input'
-    ) as HTMLTemplateElement
-    this.hostElement = document.getElementById('app') as HTMLDivElement
+    )! as HTMLTemplateElement
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
 
     const importedNode = document.importNode(this.templateElement.content, true)
     this.element = importedNode.firstElementChild as HTMLFormElement
@@ -47,11 +88,54 @@ class ProjectInput {
     this.attach()
   }
 
-  @autoBind
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    }
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    }
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!')
+      return
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault()
-
-    console.log(this.titleInputElement.value)
+    const userInput = this.gatherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput
+      console.log(title, desc, people)
+      this.clearInputs()
+    }
   }
 
   private configure() {
@@ -64,5 +148,3 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput()
-
-console.log(prjInput)
